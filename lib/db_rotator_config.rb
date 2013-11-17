082894e027589ed16552b9dfaa0c16465c73e54c
mcnelson <michael@nelsonware.com>
2013-11-17 13:23:26 -0800

    Add default values & verbose option

--- lib/db_rotator_config.rb
+++ lib/db_rotator_config.rb
@@ -10,13 +10,14 @@ class DBRotatorConfig
     mysql_command:          ['m', "Used for all database management operations.", "mysql"],
     maximum_dbs:            ['n', "Maximum number of DBs to maintain, or null to disable pruning.", "2"],
     unarchive_command:      ['u', "How to unarchive your dump to standard output.", "bzip2 -cd"],
-    unarchive_extra_pipe:   ['i',  "Any extra script(s) you want to run between unarchive & import.", "nil"],
-    reasonable_diskspace:   ['s', "Rough estimate of temporary disk space required to import a typical dump, in GB.", "nil"],
-    rails_db_yaml_path:     ['y', "Updates database name in your YAML file.", "nil"],
+    unarchive_extra_pipe:   ['i',  "Any extra script(s) you want to run between unarchive & import.", nil],
+    reasonable_diskspace:   ['s', "Rough estimate of temporary disk space required to import a typical dump, in GB.", nil],
+    rails_db_yaml_path:     ['y', "Updates database name in your YAML file.", nil],
     rails_environments:     ['e', "In conjunction with -y, which rails envs to update DB name for.", "development"],
   }
 
   REQUIRED = %i(db_prefix scp_command)
+  attr_reader :config
 
   def initialize
     @config = {}
@@ -27,10 +28,14 @@ class DBRotatorConfig
       opts.banner = "Usage: db-rotator [options]"
 
       CONFIG.each do |key, o|
-        default = o[2] ? "Default: #{o[2]}" : nil
-        opts.on("-#{o[0]}", "--#{key.to_s.gsub(/_/, '-')}", o[1], default) do |v|
-          @config = v
+        value = o[2].nil? ? "VALUE" : "[VALUE]"
+        opts.on("-#{o[0]}", "--#{key.to_s.gsub(/_/, '-')} #{value}", o[1]) do |v|
+          @config[key] = v
         end
+      end
+
+      opts.on("-v", "--verbose") do |v|
+        @config[:verbose] = v
       end
 
       opts.on_tail("-h", "--help", "Show this message") do
@@ -40,5 +45,12 @@ class DBRotatorConfig
     end
 
     parser.parse!(args)
+    add_default_values
+  end
+
+  def add_default_values
+    CONFIG.each do |key, o|
+      @config[key] ||= o[2]
+    end
   end
 end
