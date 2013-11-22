1fb4513c7ff5cd042999ca79d5ebf8c0101e74b5
mcnelson <michael@nelsonware.com>
2013-11-21 21:11:43 -0800

    Implement -g, generate config file

--- lib/db_rotator_config.rb
+++ lib/db_rotator_config.rb
@@ -8,26 +8,28 @@ class DBRotatorConfig
   FILE = ".db-rotator.yml"
 
   CONFIG = {
-    db_prefix:              [['-p', "--db-prefix PREFIX", "Database naming prefix"], nil],
-    scp_command:            [['-c', "--scp-command COMMAND", "Command to retrive dump. Receives second arg of dump path"], nil],
+    db_prefix:              [['-p', "--db-prefix PREFIX", "Database naming prefix."], nil],
+    scp_command:            [['-c', "--scp-command COMMAND", "Command to retrive dump. Receives second arg of dump path."], nil],
 
-    local_dump_destination: [['-d', "--local-destination [PATH]", "Where to put the dump, as a directory. Won't be deleted after running rotator.", "Default: /tmp"], "/tmp"],
-    mysql_command:          [['-m', "--mysql-command [COMMAND]", "Used for all database management operations.", "mysql"], "mysql"],
+    local_dump_destination: [['-d', "--local-destination [PATH]", "Where to put the dump, as a directory. The dump won't be deleted after running rotator.", "Default: /tmp"], "/tmp"],
+    mysql_command:          [['-m', "--mysql-command [COMMAND]", "Used for all database management operations.", "Default: mysql"], "mysql"],
     maximum_dbs:            [['-n', "--maximum-dbs [N]", "Maximum number of DBs to maintain, or null to disable pruning.", "Default: 2"], 2],
     unarchive_command:      [['-u', "--unarchive-command [COMMAND]", "How to unarchive your dump to standard output.", "Default: bzip2 -cd"], "bzip2 -cd"],
     unarchive_extra_pipe:   [['-i', "--extra-pipes [SCRIPT1,SCRIPT2]", Array, "Any extra script(s) you want to run between unarchive & import.", "Example: -i 'script1,script2'"], nil],
-    reasonable_diskspace:   [['-s', "--minimum-diskspace [GB]", "Rough estimate of temporary disk space required to import a typical dump, in GB.", "Default: nil"], nil],
+    reasonable_diskspace:   [['-s', "--minimum-diskspace [GB]", "Rough estimate of temporary disk space required to import a typical dump, in GB."], nil],
     rails_db_yaml_path:     [['-y', "--rails-db-yaml [PATH]", "Updates database name in your YAML file when given.", "Default: nil"], nil],
-    rails_environments:     [['-e', "--rails-db-environments [ENV1,ENV2]", Array, "In conjunction with -y, which rails envs to update DB name for.", "Default [development]"], ["development"]],
+    rails_environments:     [['-e', "--rails-db-environments [ENV1,ENV2]", Array, "In conjunction with -y, which rails envs to update DB name for. Default: development"], ["development"]],
 
-    config_file:            [['-f', "--config-file PATH", "Load configuration from .yml file"], nil],
+    config_file:            [['-f', "--config-file PATH", "Runs rotator with configuration from this .yml file."], nil],
   }
 
   REQUIRED = %i(db_prefix scp_command)
+  EXCLUDE_FROM_GENERATE_FILE = %i(config_file dump_filename)
   attr_reader :config
 
   def initialize
     @config = {}
+    @generate_output_file = nil
   end
 
   def configure
@@ -41,6 +43,12 @@ class DBRotatorConfig
       check_required
       add_default_values
       add_derived_values
+
+      if @generate_output_file
+        generate_config
+        exit
+      end
+
     rescue FileNotFoundError => e
       puts "There was a problem loading configuration: #{e.message}"
       puts cli_parser.summarize
@@ -78,6 +86,10 @@ class DBRotatorConfig
         end
       end
 
+      opts.on("-g", "--generate-config FILE", "Generates .yml config file from given arguments.") do |v|
+        @generate_output_file = v
+      end
+
       opts.on("-v", "--verbose") do |v|
         @config[:verbose] = v
       end
@@ -95,6 +107,15 @@ class DBRotatorConfig
 
   def check_required
     REQUIRED.each { |k| raise "config option '#{k.to_s} is required'" if @config[k].nil? }
+  end
+
+  def generate_config
+    File.write(@generate_output_file, config_yaml)
+  end
+
+  def config_yaml
+    require 'pry'; binding.pry
+    @config.reject { |k, _| EXCLUDE_FROM_GENERATE_FILE.include?(k) } .to_yaml
   end
 
   def add_default_values
