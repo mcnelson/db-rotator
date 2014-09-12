d3d2bad864776312ba379bf6b39d3345e5f47f70
Victor Asteinza <victor@theclymb.com>
2014-09-12 11:34:19 -0700

    Specify mysql user as root in tests.
    The tests currently do not specify a user and therefore fail if one does
    not have a default user configured.  This change specifies the user as
    root so that tests can pass without having a default user specified.

--- test/test_db_rotator.rb
+++ test/test_db_rotator.rb
@@ -1,10 +1,12 @@
 require_relative '../test/helper'
 
 describe DBRotator do
+  MYSQL_COMMAND = 'mysql -u root'
+
   after do
-    `mysql -B -e 'SHOW SCHEMAS;'`.split.each do |schema|
+    `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.split.each do |schema|
       if /^#{TEST_SCHEMA_PREFIX}/.match(schema)
-        `mysql -e "DROP SCHEMA #{schema}"`
+        `#{MYSQL_COMMAND} -e "DROP SCHEMA #{schema}"`
       end
     end
   end
@@ -23,22 +25,22 @@ describe DBRotator do
 
       Time.stub(:now, t1) do
         dbr.rotate
-        `mysql -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t1))
-        `mysql -B -e 'SHOW TABLES FROM #{dbname_with(t1)};'`.must_include("test")
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t1))
+        `#{MYSQL_COMMAND} -B -e 'SHOW TABLES FROM #{dbname_with(t1)};'`.must_include("test")
       end
 
       Time.stub(:now, t2) do
         dbr.rotate
-        `mysql -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t2))
-        `mysql -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t1))
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t2))
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t1))
       end
 
       Time.stub(:now, t3) do
         dbr.rotate
-        `mysql -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t3))
-        `mysql -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t2))
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t3))
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.must_include(dbname_with(t2))
 
-        `mysql -B -e 'SHOW SCHEMAS;'`.wont_include(dbname_with(t1))
+        `#{MYSQL_COMMAND} -B -e 'SHOW SCHEMAS;'`.wont_include(dbname_with(t1))
       end
     end
   end
